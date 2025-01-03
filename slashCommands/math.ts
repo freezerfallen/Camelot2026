2af16ec0154ad916b827bbe5bbb20e3390772142
Apollo24K <apollo24.ed@gmail.com>
2025-01-03 23:19:23 +0100

    26/91 commands

--- slashCommands/math.ts
+++ slashCommands/math.ts
@@ -1,29 +1,30 @@
 import { EmbedBuilder } from "discord.js";
 import math from 'mathjs';
 import { achievements } from "../Modules/achievements";
+import { SlashCommand } from '../types';
 
-module.exports = {
+const exportCommand: SlashCommand = {
     name: 'math',
-    description: 'see your level',
-    execute(interaction) {
-
+    async execute({ interaction }) {
         let resp;
-        const calculation = interaction.options.getString('calculation');
+        const calculation = interaction.options.getString('calculation') ?? "";
         const ephemeral = interaction.options.getBoolean('ephemeral') ?? true;
 
         try {
             resp = math.evaluate(calculation);
         } catch (e) {
             return interaction.reply({ content: "Please input a valid calculation", ephemeral });
-        };
+        }
 
         const Embed = new EmbedBuilder()
             .setTitle('Camelot Calculator')
             .setColor(0xbbffff)
             .addFields({ name: `${calculation} =`, value: `\`\`\`js\n${resp}\`\`\`` });
-        interaction.reply({ embeds: [Embed], ephemeral });
+        await interaction.reply({ embeds: [Embed], ephemeral });
 
         // Achievements
         if (resp == 42) achievements[49].check(interaction);
     },
 };
+
+export default exportCommand;
