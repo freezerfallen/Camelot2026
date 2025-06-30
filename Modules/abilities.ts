6cabbb2199318ffac26afa29b86095a44655db87
freezerfallen <kekehrawr@gmail.com>
2025-07-01 01:24:02 +0800

    2B EX fix (#90)
    * Meme buff, Urashima Rework, OnMiss add, Desc fixes
    
    * Slight Urashima desc fix
    
    * Catchup
    
    * Urashima fix
    
    * Catchup again
    
    * Catchup last
    
    * 2B&9S EX fixes
    
    * Forgot to push the Revive fix for 2B 😭
    
    * Description fixes, light ring changes
    
    * Escanor EX fix
    
    * Crit overflow fix
    
    * Frieren EX fix, Shadowpact fix
    
    * Shadowpact fix
    
    * Ring fixes / buffs
    
    * Rosalia, GintokiEX, some rings
    
    * Undo random import...
    
    * Rosalia fix
    
    * Abil - EscanorEX, Nahida, Seishirou Nagi
    
    * Guten Morgen Apollo, gl for raids~
    
    * Dalus ability, EscanorEX nerf+fix, 2b9sEX typo fix
    
    * Item changes, EscanorEX nerf, LriaEX QoL
    
    * Dalus & Kisogi's pearl item
    
    * Unintended import fix
    
    * 2BEX nerf, Desc fixes, Effect buffs, ty apo!
    
    * Desc fix
    
    * Roe Eile fix
    
    * Roe Eile fix (2)
    
    * Item change
    
    * Desc fix
    
    * Item changes
    
    * ArtemisEX fix
    
    * Fixes
    
    * Fixes (2)
    
    * Fixes (3)
    
    * 2BEX pod fix

--- Modules/abilities.ts
+++ Modules/abilities.ts
@@ -5741,7 +5741,7 @@ export const abilities: Record<number, Ability> = {
                         case "a120":
                             notice.push("\n`⚙️` Pod has been equipped with programme : **Repair**.");
                             myStats.delayedBuffs.push(new delayedBuffs(0, async (myStats, myStatsFixed, eStats, mybuff, ebuff, char, enemy, matchStats, notice, embed, user, ...list) => {
-                                if (matchStats.round % 3 === 0) {
+                                if (matchStats.round % 4 === 0) {
                                     myStats.delayedBuffs.push(new delayedBuffs(0, async (myStats, myStatsFixed, eStats, mybuff, ebuff, char, enemy, matchStats, notice, embed, user, ...list) => {
                                         const heal = Math.floor(myStats.maxhp * 0.05);
                                         addHeal(myStats, eStats, myStats, mybuff, ebuff, matchStats, notice, ``, heal, {});
@@ -5756,7 +5756,7 @@ export const abilities: Record<number, Ability> = {
                         case "a140":
                             notice.push("\n`⚙️` Pod has been equipped with programme : **Gravity**.");
                             myStats.delayedBuffs.push(new delayedBuffs(0, async (myStats, myStatsFixed, eStats, mybuff, ebuff, char, enemy, matchStats, notice, embed, user, ...list) => {
-                                if (matchStats.round % 3 === 0) {
+                                if (matchStats.round % 4 === 0) {
                                     eStats.atk -= Math.floor(eStats.atk * 0.25);
                                     eStats.md -= Math.floor(eStats.md * 0.25);
                                     eStats.def -= Math.floor(eStats.def * 0.25);
@@ -5781,7 +5781,7 @@ export const abilities: Record<number, Ability> = {
                         case "r020":
                             notice.push("\n`⚙️` Pod has been equipped with programme : **Mirage**.");
                             myStats.delayedBuffs.push(new delayedBuffs(0, async (myStats, myStatsFixed, eStats, mybuff, ebuff, char, enemy, matchStats, notice, embed, user, ...list) => {
-                                if (matchStats.round % 3 === 0) {
+                                if (matchStats.round % 4 === 0) {
                                     dealDamage(eStats, myStats, ebuff, mybuff, matchStats, notice, "`⚙️` Pod analyzed the foe! **Pod**", { atkMultiplier: 0.2, dodge: 0 });
                                     dealDamage(eStats, myStats, ebuff, mybuff, matchStats, notice, "`⚙️` Pod analyzed the foe! **Pod**", { atkMultiplier: 0.2, dodge: 0 });
                                 };
