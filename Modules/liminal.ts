93fb6d9a4c2e750f16f5ffd259171e78582ab01e
freezer <kekehrawr@gmail.com>
2025-08-14 14:46:25 +0800

    Liminal fixes

--- Modules/liminal.ts
+++ Modules/liminal.ts
@@ -55,14 +55,13 @@ export default class NightmareInfo {
 export const nightmares: NightmareInfo[] = [
     // Summer2025
     new NightmareInfo(0, "Tidalfish", "summer2025", 30_000, nightmareMobs[0], { accentColor: "#71afe6ff", preSelectedChar: 9606 }),
-    new NightmareInfo(1, "Mari the poisonbearer", "summer2025", 40_000, nightmareMobs[1], { accentColor: "#b495a4ff", preSelectedChar: 2080 }),
+    new NightmareInfo(1, "Mari the poisonbearer", "summer2025", 40_000, nightmareMobs[1], { accentColor: "#b495a4ff", preSelectedChar: 9000 }),
     new NightmareInfo(2, "Sand Golem", "summer2025", 50_000, nightmareMobs[2], { accentColor: "#e0d589ff", preSelectedChar: 16195 }),
     new NightmareInfo(3, "Luminous (alter)", "summer2025", 50_000, nightmareMobs[3], { accentColor: "#513f7cff", preSelectedChar: 10517 }),
     new NightmareInfo(4, "Bubble Captain", "summer2025", 50_000, nightmareMobs[4], { accentColor: "#5395ceff", preSelectedChar: 14909 }),
     new NightmareInfo(5, "Dalus the nightmare", "summer2025", 50_000, nightmareMobs[5], { accentColor: "#e7854cff", preSelectedChar: 12398 }),
     new NightmareInfo(6, "Solarion", "summer2025", 50_000, nightmareMobs[6], { accentColor: "#eec80aff", preSelectedChar: 17686 }),
-    // will change to shorekeeper ex id once added
-    new NightmareInfo(7, "Victoria the Dragonslayer", "summer2025", 50_000, nightmareMobs[7], { accentColor: "#c26d57ff", preSelectedChar: 1 }),
+    new NightmareInfo(7, "Victoria the Dragonslayer", "summer2025", 50_000, nightmareMobs[7], { accentColor: "#c26d57ff", preSelectedChar: 24798 }),
     new NightmareInfo(8, "Anastasia", "summer2025", 50_000, nightmareMobs[8], { accentColor: "#a641c4ff", preSelectedChar: 10528 }),
     new NightmareInfo(9, "Espathera", "summer2025", 50_000, nightmareMobs[9], { accentColor: "#504953ff", preSelectedChar: 405 }),
     new NightmareInfo(10, "Icecream", "summer2025", 50_000, nightmareMobs[10], { accentColor: "#56d79bff", preSelectedChar: 12394 }),
