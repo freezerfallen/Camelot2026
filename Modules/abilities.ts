4021c3b2d73567a6683d8920d78de35bd6d3a3cd
freezerfallen <kekehrawr@gmail.com>
2025-12-21 16:27:12 +0800

    Dalus value fix (#130)
    * Meme buff, Urashima Rework, OnMiss add, Desc fixes
    
    * Slight Urashima desc fix
    
    * Catchup
    
    * Urashima fix
    
    * Catchup again
    
    * Catchup last
    
    * 2B&9S EX fixes
    
    * Forgot to push the Revive fix for 2B 😭
    
    * Description fixes, light ring changes
    
    * Escanor EX fix
    
    * Crit overflow fix
    
    * Frieren EX fix, Shadowpact fix
    
    * Shadowpact fix
    
    * Ring fixes / buffs
    
    * Rosalia, GintokiEX, some rings
    
    * Undo random import...
    
    * Rosalia fix
    
    * Abil - EscanorEX, Nahida, Seishirou Nagi
    
    * Guten Morgen Apollo, gl for raids~
    
    * Dalus ability, EscanorEX nerf+fix, 2b9sEX typo fix
    
    * Item changes, EscanorEX nerf, LriaEX QoL
    
    * Dalus & Kisogi's pearl item
    
    * Unintended import fix
    
    * 2BEX nerf, Desc fixes, Effect buffs, ty apo!
    
    * Desc fix
    
    * Roe Eile fix
    
    * Roe Eile fix (2)
    
    * Item change
    
    * Desc fix
    
    * Item changes
    
    * ArtemisEX fix
    
    * Fixes
    
    * Fixes (2)
    
    * Fixes (3)
    
    * 2BEX pod fix
    
    * 2BEX fix, Escanor slight buff
    
    * Fragarach typo fix
    
    * Kisogi's ability, Shorekeeper changes, ItemFix
    
    * Merged existing changes
    
    * Goldenbough fix, Rukia fix
    
    * Splintered Storm fix
    
    * Sacred lifemender fix
    
    * Arlecchino ability & BoL lowerheal option
    
    * character tier changes
    
    * Item reworks/buffs, ability changes
    
    * BoL fix
    
    * Functions for shorekeeper
    
    * Functions, add multiple char
    
    * yes
    
    * Glassteeth by Drasti
    
    * Abyssal Shrimpsong fix
    
    * Commenting out Taiga Kagami ; Nerfing armor
    
    * removing
    
    * Ability changes, liminal fixes, item fixes
    
    * Liminal fixes
    
    * Item update
    
    * Image changes
    
    * Sculpture - Finality
    
    * Fontless
    
    * Vindicator & Sirenscale fix
    
    * minor desc edit
    
    * Ability and item fix/nerfs
    
    * desc fix
    
    * Finality lore edit
    
    * Sledgefist shieldbreak fix
    
    * Update admin.ts
    
    * Ability and item fixes
    
    * shieldBreak add
    
    * Ability and item fix/nerfs
    
    * catchup
    
    * Shell
    
    * dalus buff
    
    * catchup
    
    * Abil and item fix/nerfs
    
    * Dalus/Kisogi name flip
    
    * dalus and meme fix
    
    * blade cap
    
    * fix
    
    * Urashima fix
    
    * sk party fix
    
    * visual fix
    
    * Ability for Mash Burnedead, Akiko Yosano and Taiga
    
    * mana cap
    
    * Phrolova
    
    * Deflect damage
    
    * Garou EX ability, Lria Party notif fix, Rudy desc
    
    * abil edits
    
    * garou fix
    
    * Abilities, BattleUI, class desc, grappler change
    
    * desc additions
    
    * dalus value fix
    
    * update
    
    ---------
    
    Co-authored-by: Apollo24K <64652448+Apollo24K@users.noreply.github.com>

--- Modules/abilities.ts
+++ Modules/abilities.ts
@@ -2190,7 +2190,7 @@ export const abilities: Record<number, Ability> = {
         },
         party: async (pStats, myStats, eStats, mybuff, ebuff, char, enemy, matchStats, notice, embed, user, ...list) => {
             const names = matchStats.partyChars.map((e: IcharInfo) => e.name);
-            const dmgScale = names.includes("Kisogi") ? 0.15 : 0.1, critChance = names.includes("Kisogi") ? 1 : Math.random(), dmg = (eStats.def + eStats.mr < 100000) ? Math.floor(myStats.atk * dmgScale) : 0, desc = names.includes("Kisogi") ? `**✨ Kisogi**` : `<:rosie:1408505520641409127> **Rosie**`;
+            const dmgScale = names.includes("Kisogi") ? 0.1 : 0.08, critChance = names.includes("Kisogi") ? 1 : Math.random(), dmg = (eStats.def + eStats.mr < 100000) ? Math.floor(myStats.atk * dmgScale) : 0, desc = names.includes("Kisogi") ? `**✨ Kisogi**` : `<:rosie:1408505520641409127> **Rosie**`;
             // Dalus unique effects
             if (!names.includes("Kisogi")) {
                 eStats.def -= Math.min(Math.floor(eStats.def * 0.2), 660);
